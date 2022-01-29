30ccd10811cbf21e7a4fe8e97a65a54c36183855
olaf <olaf@olafui-MacBookAir.local>
2022-01-29 23:11:08 +0900

    feat: post header 스타일링 추가

--- src/pages/[year]/[...slugs].tsx
+++ src/pages/[year]/[...slugs].tsx
@@ -1,23 +1,44 @@
 import React from 'react'
 import { getMDXComponent } from 'mdx-bundler/client'
+import classNames from 'classnames/bind'
+
+import styles from './Post.module.scss'
 
 import { Post, Slug } from '$types/post'
 import { getAllPosts, getPost } from '$utils/posts'
 import PostSEO from '$components/shared/PostSEO'
 
+const cx = classNames.bind(styles)
+
 function PostDetail({ post, code }: { post: Post; code: string }) {
   const Component = React.useMemo(() => getMDXComponent(code), [code])
 
   const {
-    frontMatter: { title, description, thumbnailImg },
+    frontMatter: { title, description, thumbnailImg, date },
   } = post
 
   return (
-    <>
+    <article className={cx('article')}>
       <PostSEO title={title} description={description} thumbnail={thumbnailImg} />
-      <div>{post.frontMatter.title}</div>
-      <Component />
-    </>
+      <header>
+        <div className={cx('gradient-top')} />
+        <div className={cx('wrap-img')}>
+          <img src={thumbnailImg} alt="" />
+        </div>
+        <div className={cx('gradient-bottom')} />
+        <div className={cx('wrap-title')}>
+          <div className={cx('txt-title')}>{post.frontMatter.title}</div>
+          <time>{date}</time>
+        </div>
+      </header>
+      <div className={cx('wrap-content')}>
+        <div className={cx('wrap-title')}>
+          <div className={cx('txt-title')}>{post.frontMatter.title}</div>
+          <time>{date}</time>
+        </div>
+        <Component />
+      </div>
+    </article>
   )
 }
 
