42728f08facf48a882dec2c96ace07e2c8fb93e3
appear <appear.ko@gmail.com>
2022-02-04 14:12:00 +0900

    docs: 테스트 코드 글 추가

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 export default {
   author: 'olaf',
   title: 'dev.olaf',
-  summary: '다양한 이야기를 나눠요',
+  summary: '떡볶이와 여행을 좋아하는 FE 개발자입니다.',
   thumbnail: '/assets/images/thumbnail/main.png',
   url: 'https://olaf-blog.vercel.app', // TODO 변경예정
   link: {
