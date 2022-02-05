b61e9cff0fdb6c93ef07ae1a6425c338ed7e646f
appear <appear.ko@gmail.com>
2022-02-05 12:46:02 +0900

    feat: google search meta 추가

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,6 +8,10 @@ export default class MyDocument extends Document {
       <Html lang="ko">
         <Head>
           <meta charSet="utf-8" />
+          <meta
+            name="google-site-verification"
+            content="GVt_B0UKzXR4zZRWh6mzL8X17B-yEjV2DKKwML7D-oY"
+          />
           <meta name="author" content={config.author} />
           <meta property="og:type" content="website" />
           {typeof window !== 'undefined' && (
