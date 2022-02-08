97c0a79c754f66da995ad3c461a8832b458d28c2
appear <appear.ko@gmail.com>
2022-02-09 00:01:27 +0900

    feat: 내 소개 페이지 추가

--- src/config.ts
+++ src/config.ts
@@ -7,4 +7,14 @@ export default {
   link: {
     linkedIn: 'https://www.linkedin.com/in/%EC%84%9D%EC%A7%84-%EA%B3%A0-a0a5a5128',
   },
+  about: {
+    name: '고석진',
+    intro: `여행과 떡볶이를 좋아하는 FE 개발자입니다.\n흥미로운 UI/UX, 디자인시스템에 관심이 많습니다.`,
+    image: '/assets/images/about/me.jpeg',
+    social: {
+      linkedIn: 'https://www.linkedin.com/in/%EC%84%9D%EC%A7%84-%EA%B3%A0-a0a5a5128/',
+      github: 'https://github.com/appear',
+      instargram: 'https://www.instagram.com/jinnnn_07/',
+    },
+  },
 } as const
